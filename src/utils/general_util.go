1a1395279f6f67a59fa3aab1c16b90dfaf894f46
ahbkc <yinqilei@outlook.com>
2018-09-10 13:38:10 +0800

    增加getMapVal方法，修改了所有从jsonMap取值的操作

--- src/utils/general_util.go
+++ src/utils/general_util.go
@@ -1,23 +1,19 @@
 package utils
 
 import (
-	"os"
-	"regexp"
-	"errors"
-	"strings"
+	"bytes"
 	"encoding/json"
+	"errors"
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 	"log"
-	"github.com/jinzhu/gorm"
-	"time"
-	"sync"
 	"net/http"
+	"os"
 	"structs"
-	"github.com/gorilla/mux"
-	"math/rand"
-	"strconv"
-	"bytes"
+	"sync"
 	"syscall"
+	"time"
 )
 
 const (
@@ -29,13 +25,13 @@ const (
 )
 
 var (
-	ConfigureMap map[string]map[string]string
-	LanguageMap  map[string]map[string]string
+	ConfigureMap map[string]string
+	LanguageMap  map[string]string
 	Dir          string
 	errs         error
 	ch           chan string
 	Sessions     structs.Session
-	Method       = make(map[string]string)
+	Method       = map[string]string{"GET": ".html", "POST": "_ajax"}
 	IdKeyD       string
 	StatikFS     http.FileSystem
 )
@@ -46,8 +42,7 @@ type General struct {
 func init() {
 	Dir, errs = os.Getwd() //get current path
 	CheckStartError(errs)
-	Method["GET"] = ".html"
-	Method["POST"] = "_ajax"
+	ReadConfigure()        //init read .json file
 }
 
 //new General struct
@@ -57,7 +52,6 @@ func NewGeneral() *General {
 
 //Read Json Configuration file
 func ReadConfigure() {
-
 	//configure
 	configure, err := os.OpenFile(Dir + configureFileName, syscall.O_RDONLY, 0666)
 	CheckStartError(err)
@@ -66,7 +60,7 @@ func ReadConfigure() {
 	CheckStartError(err)
 
 	//language
-	language, err := os.OpenFile(Dir+ConfigureMap["FILE_PATH"]["LANGUAGE_FILE"] + "/" + ConfigureMap["BASE"]["LANGUAGE"] + ".json", syscall.O_RDONLY, 0666)
+	language, err := os.OpenFile(Dir + getMapVal("LANGUAGE_FILE") + "/" + getMapVal("LANGUAGE") + ".json", syscall.O_RDONLY, 0666)
 	CheckStartError(err)
 
 	err = json.NewDecoder(language).Decode(&LanguageMap)
@@ -81,10 +75,6 @@ func CheckStartError(err error) {
 	}
 }
 
-//Check runtime error and return
-func HandlerRuntimeErr(err error) {
-}
-
 //check return err
 func CheckErr(err error) {
 	if err != nil {
@@ -92,71 +82,24 @@ func CheckErr(err error) {
 	}
 }
 
-//Interception string
-//sep only allowed | \ , . / @ $ #
-func CutString(param, sep string, num, flag int) (v string, err error) {
-	if len(param) <= 0 {
-		return param, errors.New("error: length is 0")
-	}
-	//check whether the separator is legal
-	regex := regexp.MustCompile("[|,.$@/]")
-	if !regex.MatchString(sep) {
-		return param, errors.New("error: Delimiter error, please enter the correct separator")
-	}
-	count := strings.Count(param, sep)
-	if count <= 0 {
-		return param, errors.New("error: Does not contain specified characters")
-	}
-	if num > count {
-		return param, errors.New("error: Specifies the separator subscript exceeds")
-	}
-	if num == 0 {
-		num = num + 1
-	}
-	params := []rune(param)
-	var index = 0
-	if flag == 0 {
-		//positive
-		for k, v := range params {
-			if string(v) == sep && num > 0 {
-				num -= 1
-				if num == 0 {
-					index = k
-					break
-				}
-			}
-		}
-	} else if flag == -1 {
-
-	}
-	return string(params[index+1 : len(params)]), nil
-}
-
 //start method
 //start http server and static resource httpServer
 func (g *General) Run(rou *mux.Router) {
-	ReadConfigure()        //init read .json file
-	dbMap := ConfigureMap["DATABASE"]
-	if len(dbMap) <= 0 {
-		log.Println("error: Database configuration is empty")
-	}
-	db, err := gorm.Open(ConfigureMap["DATABASE"]["dialect"], Dir + ConfigureMap["DATABASE"]["db_path"])
+	db, err := gorm.Open(getMapVal("dialect"), Dir + getMapVal("db_path"))
 	CheckStartError(err)
 	err = db.Close()
 	CheckStartError(err)
 
 	//http configure port...
-	baseMap := ConfigureMap["BASE"]
+	baseMap := getMapVal("PORT")
 	if len(baseMap) <= 0 {
 		log.Println("error: Base configuration is empty")
 	}
-	//route.PathPrefix("/resource/").Methods("GET").
-	//	Handler(http.FileServer(http.Dir(Dir)))
 	var lock sync.WaitGroup
 	go func() {
 		lock.Add(1)
 		ch = make(chan string)
-		ch <- http.ListenAndServe(baseMap["PORT"], rou).Error()
+		ch <- http.ListenAndServe(baseMap, rou).Error()
 		lock.Done()
 	}()
 
@@ -167,64 +110,35 @@ func (g *General) Run(rou *mux.Router) {
 		}
 	case <-time.After(time.Second * 2):
 		{
-			log.Println("http server Successful startup... Question port is ", baseMap["PORT"])
+			log.Println("http server Successful startup... Question port is ", baseMap)
 		}
 	}
-
 	lock.Wait() //wait done
 }
 
 func CheckToken(token string) bool {
-	dbMap := ConfigureMap["DATABASE"]
-	db, err := gorm.Open(dbMap["dialect"], Dir+dbMap["db_path"])
+	db, err := gorm.Open(getMapVal("dialect"), Dir+getMapVal("db_path"))
 	if err != nil {
-		HandlerRuntimeErr(err)
 		return false
 	}
 	defer db.Close()
-	uMap := ConfigureMap["ADMIN"]
-	if uMap["TOKEN"] == token {
+	if getMapVal("TOKEN") == token {
 		return true
 	}
 	return false
 }
 
-func GetRandCode() string {
-	code := ""
-	for i := 0; i < 6; i ++ {
-		code += strconv.Itoa(rand.Intn(10))
-	}
-	return code
-}
-
 func GetMenuList(flag uint8) []structs.Menu {
 	var menus = []structs.Menu{
-		{Name: LanguageMap["PAGE"]["MENU_INDEX"], Url: "/admin/index.html"},
-		{Name: LanguageMap["PAGE"]["MENU_CATEGORY"], Url: "/admin/category/index.html",},
-		{Name: LanguageMap["PAGE"]["MENU_ARTICLE"], Url: "/admin/article/index.html"},
+		{Name: getMapVal("MENU_INDEX"), Url: "/admin/index.html"},
+		{Name: getMapVal("MENU_CATEGORY"), Url: "/admin/category/index.html",},
+		{Name: getMapVal("MENU_ARTICLE"), Url: "/admin/article/index.html"},
 	}
 	menus[flag].Class = "layui-this"
 	return menus
 }
 
-func GetCommonParamMap() map[string]interface{} {
-	var data = make(map[string]interface{})
-	data["Title"] = LanguageMap["PAGE"]["ADMIN_LOGIN_TITLE"]
-	data["JsLoginErrMsg"] = LanguageMap["API_MESSAGE"]["LOGIN_FAILED"]
-	data["ConsoleName"] = LanguageMap["PAGE"]["ADMIN_INDEX_CONSOLE_NAME"]
-	data["UserName"] = "ahbkc"
-	data["AjaxErrorMsg"] = LanguageMap["PAGE"]["AJAX_ERROR_TIPS_MESSAGE"]
-	data["Welcome"] = LanguageMap["PAGE"]["ADMIN_FOOTER_MESSAGE"]
-	data["ConfirmLogoutTips"] = LanguageMap["PAGE"]["CONFIRM_LOGOUT_TIPS"]
-	data["LogoutName"] = "退出"
-	return data
-}
-
-func GetCurDateFolder() string {
-	now := time.Now()
-	return strconv.Itoa(now.Year()) + "/" + now.Month().String() + "/" + strconv.Itoa(now.Day()) + "/"
-}
-
+//Read files from statik
 func ReadHTMLFileToString(path string) string {
 	file, err := StatikFS.Open(path)
 	CheckErr(err)
@@ -232,4 +146,14 @@ func ReadHTMLFileToString(path string) string {
 	buf := new(bytes.Buffer)
 	buf.ReadFrom(file)
 	return buf.String()
+}
+
+func getMapVal(s string) string {
+	if v1 := ConfigureMap[s]; v1 != "" {
+		return v1
+	}else if v2 := LanguageMap[s]; v2 != "" {
+		return v2
+	}else {
+		panic(errors.New("no Match Value"))
+	}
 }
