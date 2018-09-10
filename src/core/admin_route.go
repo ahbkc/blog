e58f12ac68549aec6aaaf24cde14f518783fbabd
ahbkc <yinqilei@outlook.com>
2018-09-10 11:42:11 +0800

    简化josnMap的值获取方式

--- src/core/admin_route.go
+++ src/core/admin_route.go
@@ -14,21 +14,21 @@ import (
 
 //admin login page --get
 func AdminLoginGet(w http.ResponseWriter, r *http.Request) {
-	if utils.ConfigureMap["BASE"]["ENVIRONMENT"] == "PRODUCT" {
+	if GetMapVal("ENVIRONMENT") == "PRODUCT" {
 		t, err = template.New("admin_login").Parse(utils.ReadHTMLFileToString(utils.AdminHtmlPath + "adminLogin.html"))
 	} else {
 		//读取.html文件  DEVELOP
 		t, err = template.ParseFiles(GetFilePath("adminLogin.html"))
 	}
 	utils.CheckErr(err)
-	t.Execute(w, utils.GetCommonParamMap())
+	t.Execute(w, ComADMRtnVal())
 }
 
 //admin login page --post
 func AdminLoginPost(w http.ResponseWriter, r *http.Request) {
 	//先判断session 是否为空
 	if utils.Sessions.Name != "" {
-		json.NewEncoder(w).Encode(structs.ResData{Code: "-99", Msg: utils.LanguageMap["API_MESSAGE"]["AccountHasBeenLoggedIn"]})
+		json.NewEncoder(w).Encode(structs.ResData{Code: "-99", Msg: GetMapVal("AccountHasBeenLoggedIn")})
 		return
 	}
 
@@ -38,44 +38,43 @@ func AdminLoginPost(w http.ResponseWriter, r *http.Request) {
 	password := r.FormValue("password")
 	verifyCode := r.FormValue("verifyCode")
 	if len(userName) <= 0 || len(password) <= 0 || len(verifyCode) <= 0 {
-		json.NewEncoder(w).Encode(structs.ResData{Code: "-98", Msg: utils.LanguageMap["API_MESSAGE"]["PARAMETERS_CANNOT_BE_EMPTY"]})
+		json.NewEncoder(w).Encode(structs.ResData{Code: "-98", Msg: GetMapVal( "PARAMETERS_CANNOT_BE_EMPTY")})
 		return
 	}
 
 	//verify code
 	result := base64Captcha.VerifyCaptcha(utils.IdKeyD, verifyCode)
 	if !result {
-		json.NewEncoder(w).Encode(structs.ResData{Code: "-99", Msg: utils.LanguageMap["API_MESSAGE"]["VerifyCode_Error"]})
+		json.NewEncoder(w).Encode(structs.ResData{Code: "-99", Msg: GetMapVal("VerifyCode_Error")})
 		return
 	}
 	m := md5.New()
 	_, err = m.Write([]byte(password))
 	utils.CheckErr(err)
 	cipherStr := m.Sum(nil)
-	name := utils.ConfigureMap["ADMIN"]["USER_NAME"]
-	passwd := utils.ConfigureMap["ADMIN"]["PASSWORD"]
+	name := GetMapVal( "USER_NAME")
+	passwd := GetMapVal("PASSWORD")
 	if userName != name || hex.EncodeToString(cipherStr) != passwd {
-		json.NewEncoder(w).Encode(structs.ResData{Code: "-99", Msg: utils.LanguageMap["API_MESSAGE"]["USERNAME_OR_PASSWORD_IS_INCORRECT"]})
+		json.NewEncoder(w).Encode(structs.ResData{Code: "-99", Msg: GetMapVal("USERNAME_OR_PASSWORD_IS_INCORRECT")})
 		return
 	}
 	c := http.Cookie{
 		Name:     "hhhhh_cookie",
-		Value:    utils.ConfigureMap["ADMIN"]["TOKEN"],
+		Value:    GetMapVal("TOKEN"),
 		HttpOnly: true,
 	}
 	w.Header().Set("Set-Cookie", c.String())
 	//save session
 	utils.Sessions.Name = "hhhhh_cookie"
-	utils.Sessions.Value = utils.ConfigureMap["ADMIN"]["TOKEN"]
+	utils.Sessions.Value = GetMapVal("TOKEN")
 	utils.Sessions.LoginTime = time.Now()
-	json.NewEncoder(w).Encode(structs.ResData{Code: "100", Msg: utils.LanguageMap["API_MESSAGE"]["LOGIN_SUCCESS"], Data: "/admin/index.html"})
+	json.NewEncoder(w).Encode(structs.ResData{Code: "100", Msg: GetMapVal("LOGIN_SUCCESS"), Data: "/admin/index.html"})
 	return
 }
 
 //admin index page  --get
 func AdminIndexGet(w http.ResponseWriter, r *http.Request) {
-	//var tmpl *template.Template
-	if utils.ConfigureMap["BASE"]["ENVIRONMENT"] == "PRODUCT" {
+	if GetMapVal("ENVIRONMENT") == "PRODUCT" {
 		t, err = template.New("admin_index").Parse(utils.ReadHTMLFileToString(utils.AdminHtmlPath + "adminIndex.html"))
 	} else {
 		//读取.html文件  DEVELOP
@@ -83,12 +82,7 @@ func AdminIndexGet(w http.ResponseWriter, r *http.Request) {
 			GetFilePath("admin_side_block.tmpl"), GetFilePath("admin_head_block.tmpl"), GetFilePath("admin_footer_block.tmpl"))
 	}
 	utils.CheckErr(err)
-	data := utils.GetCommonParamMap()
-	data["Menus"] = utils.GetMenuList(0)
-	data["Article"] = 100
-	data["Category"] = 100
-	data["Comment"] = 100
-	t.Execute(w, data)
+	t.Execute(w, ComADMRtnVal("Menus", utils.GetMenuList(0), "Article", 100, "Category", 100, "Comment", 100))
 	return
 }
 
@@ -97,6 +91,6 @@ func AdminLogout(w http.ResponseWriter, r *http.Request) {
 	//remove session and cookie
 	utils.Sessions.Name = ""
 	utils.Sessions.Value = ""
-	json.NewEncoder(w).Encode(structs.ResData{Code: "100", Msg: utils.LanguageMap["API_MESSAGE"]["EXECUTION_SUCCESS"], Data: "/admin/login.html"})
+	json.NewEncoder(w).Encode(structs.ResData{Code: "100", Msg: GetMapVal("EXECUTION_SUCCESS"), Data: "/admin/login.html"})
 	return
 }
