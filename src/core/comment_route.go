4a1ba399fc965636e86b7faaa66ea2e20b2b1f15
ahbkc <yinqilei@outlook.com>
2018-09-16 19:01:01 +0800

    优化参数获取

--- src/core/comment_route.go
+++ src/core/comment_route.go
@@ -11,46 +11,25 @@ import (
 
 //comment article
 func CommentLeaveMsg(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	if len(vars) <= 0 {
-		json.NewEncoder(w).Encode(structs.ResData{Code: "-98", Msg: GetMapVal("PARAMETERS_CANNOT_BE_EMPTY")})
-		return
-	}
-
-	err := r.ParseForm()
+	var comment structs.Comment
+	data, err := json.Marshal(mux.Vars(r))
 	check(err)
-	//判断请求值
-	name := r.FormValue("name")
-	mail := r.FormValue("mail")
-	address := r.FormValue("address")
-	leaveMsg := r.FormValue("leaveMsg")
+	check(json.Unmarshal(data, &comment))
+	data = paramJson(r)
+	check(json.Unmarshal(data, &comment))
 
-	//判断是否有值
-	if len(name) <= 0 || len(mail) <= 0 || len(address) <= 0 || len(leaveMsg) <= 0 {
+	if len(comment.CommentUserName) <= 0 || len(comment.CommentUserEmail) <= 0 || len(comment.CommentUserAddress) <= 0 || len(comment.CommentUserContent) <= 0 {
 		json.NewEncoder(w).Encode(structs.ResData{Code: "-98", Msg: GetMapVal("PARAMETERS_CANNOT_BE_EMPTY")})
 		return
 	}
-
-	id := vars["id"]
 	db := connect()
-	check(err)
-	//disabled mores table
-	db.SingularTable(true)
 	defer db.Close()
 
-	uuids, err := uuid.NewV4() //general uuids value
+	uid, err := uuid.NewV4()
 	check(err)
-	//保存评论
-	err = db.Save(&structs.Comment{
-		Id:uuids.String(),
-		CommentUserName: name,
-		CommentUserEmail: mail,
-		CommentUserAddress: address,
-		CommentUserContent: leaveMsg,
-		CreatedAt: time.Now().Format("2006-01-02 15:04:05"),
-		RelevancyId: id,
-	}).Error
-	check(err)
+	comment.Id = uid.String()
+	comment.CreatedAt = time.Now().Format("2006-01-02 15:04:05")
+	check(db.Save(&comment).Error)
 	json.NewEncoder(w).Encode(structs.ResData{Code: "100", Msg: GetMapVal("EXECUTION_SUCCESS")})
 	return
 }
