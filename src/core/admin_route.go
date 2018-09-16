0c33dadd57514db314b6e15363b7b46eefa14840
ahbkc <yinqilei@outlook.com>
2018-09-16 18:46:45 +0800

    优化cookie, 增加清除cookie

--- src/core/admin_route.go
+++ src/core/admin_route.go
@@ -1,13 +1,10 @@
 package core
 
 import (
-	"crypto/md5"
-	"encoding/hex"
 	"encoding/json"
 	"github.com/mojocn/base64Captcha"
 	"net/http"
 	"structs"
-	"time"
 	"utils"
 )
 
@@ -18,50 +15,30 @@ func AdminLoginGet(w http.ResponseWriter, r *http.Request) {
 	t.Execute(w, ComADMRtnVal("Title", GetMapVal("ADMIN_LOGIN_PAGE")))
 }
 
-//admin login page --post
+//admin login page --ajax post
 func AdminLoginPost(w http.ResponseWriter, r *http.Request) {
-	//先判断session 是否为空
 	if utils.Sessions.Name != "" {
 		json.NewEncoder(w).Encode(structs.ResData{Code: "-99", Msg: GetMapVal("AccountHasBeenLoggedIn")})
 		return
 	}
 
-	err := r.ParseForm()
-	check(err)
-	userName := r.FormValue("userName")
-	password := r.FormValue("password")
-	verifyCode := r.FormValue("verifyCode")
-	if len(userName) <= 0 || len(password) <= 0 || len(verifyCode) <= 0 {
-		json.NewEncoder(w).Encode(structs.ResData{Code: "-98", Msg: GetMapVal( "PARAMETERS_CANNOT_BE_EMPTY")})
-		return
-	}
-
-	//verify code
-	result := base64Captcha.VerifyCaptcha(utils.IdKeyD, verifyCode)
+	var user structs.User
+	data := paramJson(r)
+	check(json.Unmarshal(data, &user))
+	result := base64Captcha.VerifyCaptcha(utils.IdKeyD, user.VerifyCode)
 	if !result {
 		json.NewEncoder(w).Encode(structs.ResData{Code: "-99", Msg: GetMapVal("VerifyCode_Error")})
 		return
 	}
-	m := md5.New()
-	_, err = m.Write([]byte(password))
-	check(err)
-	cipherStr := m.Sum(nil)
 	name := GetMapVal( "USER_NAME")
-	passwd := GetMapVal("PASSWORD")
-	if userName != name || hex.EncodeToString(cipherStr) != passwd {
+	password := GetMapVal("PASSWORD")
+	if user.UserName != name || user.GetMd5Pwd() != password {
 		json.NewEncoder(w).Encode(structs.ResData{Code: "-99", Msg: GetMapVal("USERNAME_OR_PASSWORD_IS_INCORRECT")})
 		return
 	}
-	c := http.Cookie{
-		Name:     "hhhhh_cookie",
-		Value:    GetMapVal("TOKEN"),
-		HttpOnly: true,
-	}
+	c := utils.NewCookie(GetMapVal("COOKIE_NAME"), GetMapVal("TOKEN"), true)
 	w.Header().Set("Set-Cookie", c.String())
-	//save session
-	utils.Sessions.Name = "hhhhh_cookie"
-	utils.Sessions.Value = GetMapVal("TOKEN")
-	utils.Sessions.LoginTime = time.Now()
+	utils.SetSession(c.Name, c.Value)
 	json.NewEncoder(w).Encode(structs.ResData{Code: "100", Msg: GetMapVal("LOGIN_SUCCESS"), Data: "/admin/index.html"})
 	return
 }
@@ -69,16 +46,14 @@ func AdminLoginPost(w http.ResponseWriter, r *http.Request) {
 //admin index page  --get
 func AdminIndexGet(w http.ResponseWriter, r *http.Request) {
 	t = initTmpl("adminIndex.html")
-	check(err)
 	t.Execute(w, ComADMRtnVal("Menus", utils.GetMenuList(0), "Article", 100, "Category", 100, "Comment", 100))
 	return
 }
 
 //admin logout
 func AdminLogout(w http.ResponseWriter, r *http.Request) {
-	//remove session and cookie
-	utils.Sessions.Name = ""
-	utils.Sessions.Value = ""
+	c := utils.RemoveSession()  //del session and cookie
+	w.Header().Set("Set-Cookie", c.String())
 	json.NewEncoder(w).Encode(structs.ResData{Code: "100", Msg: GetMapVal("EXECUTION_SUCCESS"), Data: "/admin/login.html"})
 	return
 }
